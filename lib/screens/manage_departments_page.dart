b373b8cecd475e9ddf24a57a3f3836adb1ae5574
gd-salle <gerardojr.salle@gmail.com>
2024-06-03 00:26:06 +0800

    Department CRUD done

--- lib/screens/manage_departments_page.dart
+++ lib/screens/manage_departments_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../services/api_service.dart';
+import '../services/department_api_service.dart';
 import '../models/department.dart';
 import '../widgets/sidebar.dart';
 
@@ -15,7 +15,7 @@ class _ManageDepartmentsPageState extends State<ManageDepartmentsPage> {
   @override
   void initState() {
     super.initState();
-    futureDepartments = ApiService.fetchDepartments();
+    futureDepartments = DepartmentApiService.fetchDepartments();
   }
 
   void _showAddDepartmentDialog() {
@@ -24,12 +24,12 @@ class _ManageDepartmentsPageState extends State<ManageDepartmentsPage> {
       builder: (context) => AddDepartmentDialog(
         onConfirm: (String departmentName) async {
           try {
-            await ApiService.addDepartment(departmentName);
+            await DepartmentApiService.addDepartment(departmentName);
             setState(() {
-              futureDepartments = ApiService.fetchDepartments();
+              futureDepartments = DepartmentApiService.fetchDepartments();
             });
             Navigator.of(context).pop();
-            _showSuccessDialog();
+            _showSuccessDialog('The department has been successfully added.');
           } catch (error) {
             print('Error adding department: $error');
             Navigator.of(context).pop();
@@ -40,12 +40,35 @@ class _ManageDepartmentsPageState extends State<ManageDepartmentsPage> {
     );
   }
 
-  void _showSuccessDialog() {
+  void _showEditDepartmentDialog(String currentName) {
+    showDialog(
+      context: context,
+      builder: (context) => EditDepartmentDialog(
+        currentName: currentName,
+        onConfirm: (String newDepartmentName) async {
+          try {
+            await DepartmentApiService.updateDepartment(currentName, newDepartmentName);
+            setState(() {
+              futureDepartments = DepartmentApiService.fetchDepartments();
+            });
+            Navigator.of(context).pop();
+            _showSuccessDialog('The department has been successfully updated.');
+          } catch (error) {
+            print('Error updating department: $error');
+            Navigator.of(context).pop();
+            _showErrorDialog(error.toString());
+          }
+        },
+      ),
+    );
+  }
+
+  void _showSuccessDialog(String message) {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
         title: Text('Success'),
-        content: Text('The department has been successfully added.'),
+        content: Text(message),
         actions: [
           TextButton(
             onPressed: () {
@@ -57,6 +80,7 @@ class _ManageDepartmentsPageState extends State<ManageDepartmentsPage> {
       ),
     );
   }
+
 
   void _showErrorDialog(String error) {
     showDialog(
@@ -82,26 +106,41 @@ class _ManageDepartmentsPageState extends State<ManageDepartmentsPage> {
     });
   }
 
-  void _editDepartment(int departmentId, String name) async{
-    try{
-      await ApiService.editDepartment(departmentId, name);
+  void _deleteDepartment(String name) async {
+    try {
+      await DepartmentApiService.deleteDepartment(name);
       setState(() {
-        futureDepartments = ApiService.fetchDepartments();
+        futureDepartments = DepartmentApiService.fetchDepartments();
       });
-    } catch(error) {
+      _showSuccessDialog('The department has been successfully deleted.');
+    } catch (error) {
       print('Error deleting department: $error');
     }
   }
 
-  void _deleteDepartment(String name) async {
-    try {
-      await ApiService.deleteDepartment(name);
-      setState(() {
-        futureDepartments = ApiService.fetchDepartments();
-      });
-    } catch (error) {
-      print('Error deleting department: $error');
-    }
+  void _showDeleteConfirmationDialog(String name) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Confirm Deletion'),
+        content: Text('Are you sure you want to delete the department "$name"?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              _deleteDepartment(name);
+            },
+            child: Text('Delete'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -191,12 +230,12 @@ class _ManageDepartmentsPageState extends State<ManageDepartmentsPage> {
                                 children: [
                                   IconButton(
                                     icon: Icon(Icons.delete, color: Colors.red),
-                                    onPressed: () => _deleteDepartment(departments[index].name),
+                                    onPressed: () => _showDeleteConfirmationDialog(departments[index].name),
                                   ),
                                   IconButton(
                                     icon: Icon(Icons.edit, color: Colors.red),
                                     onPressed: () {
-                                      // Implement edit functionality here
+                                      _showEditDepartmentDialog(departments[index].name);
                                     },
                                   ),
                                 ],
@@ -282,6 +321,103 @@ class AddDepartmentDialog extends StatelessWidget {
                 SizedBox(width: 8),
                 Text(
                   'Add New Department',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Department Name',
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 140, // Adjust the width as needed
+                  height: 40, // Adjust the height as needed
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.red),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 140, // Adjust the width as needed
+                  height: 40, // Adjust the height as needed
+                  child: ElevatedButton(
+                    onPressed: () {
+                      onConfirm(_controller.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    child: Text(
+                      'Confirm',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EditDepartmentDialog extends StatelessWidget {
+  final String currentName;
+  final Function(String) onConfirm;
+  final TextEditingController _controller;
+
+  EditDepartmentDialog({
+    required this.currentName,
+    required this.onConfirm,
+  }) : _controller = TextEditingController(text: currentName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8, // Adjust the width as needed
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 15,
+                  height: 60,
+                  color: Colors.red,
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'Edit Department',
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
